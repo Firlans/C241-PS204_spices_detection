95177022218f570c0faf16541324a818a76090fc
Firlan <106174310+Firlans@users.noreply.github.com>
2024-06-05 01:12:51 +0700

    Delete src directory

--- src/services/db.js
+++
@@ -1,9 +0,0 @@
-const {Firestore} = require('@google-cloud/firestore');
-
-// Create a new client
-const firestore = new Firestore({
-  projectId: process.env.ID_PROJECT,
-  keyFilename: process.env.SERVICE_ACCOUNT
-});
-
-module.exports = firestore;
