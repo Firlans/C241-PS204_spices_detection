f948675f8d26f5b60edb8b951a8015aab38464db
Firlan <106174310+Firlans@users.noreply.github.com>
2024-06-05 01:16:17 +0700

    Add files via upload

---
+++ CC/src/services/generateToken.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const generateToken = (data) => {
+  const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+
+module.exports = generateToken;
