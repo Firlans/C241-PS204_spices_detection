8cc9434ae959c3c8806843442cc6d067c69020d3
Firlan <106174310+Firlans@users.noreply.github.com>
2024-06-05 01:11:42 +0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,16 @@
+"use strict";
+require("dotenv").config()
+
+const express = require("express");
+
+const routes = require("./routes/userRoutes");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(routes);
+
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
---
+++ src/services/storeData.js
@@ -0,0 +1,52 @@
+const firestore = require("./db");
+
+const addUser = async (data) => {
+  try {
+    const docRef = await firestore.collection("users").add(data);
+    console.log("Document written with ID: ", docRef.id);
+    return docRef;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
+
+const updateUser = async (userId, userData) => {
+  try {
+    const docRef = await firestore.collection("users").doc(userId).update({
+      name: userData.name,
+      password : userData.password,
+      phoneNumber: userData.phoneNumber,
+      idAvatar : userData.idAvatar,
+      updatedAt: new Date().toISOString()
+    });
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
+
+const getUsers = async () => {
+  try {
+    const usersCollection = firestore.collection("users");
+    const snapshot = await usersCollection.get();
+
+    if (snapshot.empty) {
+      console.log("No matching documents.");
+      return [];
+    }
+
+    const users = [];
+    snapshot.forEach((doc) => {
+      users.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    return users;
+  } catch (error) {
+    console.error("Error getting documents: ", error);
+    throw error;
+  }
+};
+
+module.exports = { addUser, getUsers, updateUser };
