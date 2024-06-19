b9b6d3260a7c1dda928e0774142328eadbc4e470
Firlan <106174310+Firlans@users.noreply.github.com>
2024-06-19 19:45:24 +0700

    Delete CC/src directory

--- CC/src/services/generateToken.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-
-const generateToken = (data) => {
-  const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "720h" });
-  return token;
-};
-
-module.exports = generateToken;
